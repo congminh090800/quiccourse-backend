861243ff014e665dc1e3459ab3b123b6a7b0b093
congminh090800@gmail.com <congminh090800@gmail.com>
2021-11-11 14:03:53 +0700

    schema change

--- models/admin.js
+++ models/admin.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const adminSchema = new Schema({
-  userId: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
+const adminSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    deleted_flag: { type: Boolean, default: false },
   },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  deleted_flag: { type: Boolean, default: false },
-});
+  { timestamps: true }
+);
 
 const Admin = mongoose.model("Admin", adminSchema);
 module.exports = Admin;
--- models/user.js
+++ models/user.js
@@ -2,45 +2,46 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const { validatePhone } = require("lib/regex-helpers");
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    required: true,
-    min: 13,
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      // trim: true,
+      // lowercase: true,
+      // unique: true,
+      // required: true,
+      min: 13,
+    },
+    password: {
+      type: String,
+      // required: true,
+      min: 6,
+    },
+    phone: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      // required: true,
+      validate: [validatePhone, "Please fill a valid phone number"],
+    },
+    name: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 100,
+    },
+    birthDate: { type: Date, default: Date.now },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+    },
+    avatar: {
+      type: String,
+    },
+    deleted_flag: { type: Boolean, default: false },
   },
-  password: {
-    type: String,
-    required: true,
-    min: 6,
-  },
-  phone: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    required: true,
-    validate: [validatePhone, "Please fill a valid phone number"],
-  },
-  name: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 100,
-  },
-  birthDate: { type: Date, default: Date.now },
-  gender: {
-    type: String,
-    enum: ["male", "female"],
-  },
-  avatar: {
-    type: String,
-  },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  deleted_flag: { type: Boolean, default: false },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
